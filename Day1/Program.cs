c51534f5f8e302e1fe4ba80a93278f4aa3050959
Anton Månsson <anton.arrod@gmail.com>
2019-12-11 15:24:58 +0100

    First day
    First days challenges

---
+++ Day1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using AdventOfCode.dayOne;
+namespace AdventOfCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            dayOne.dayOne test = new dayOne.dayOne();
+            List<string> fuelInput = test.ImportInput();
+            Console.WriteLine(test.calculateFuelPartTwo(fuelInput));
+        }
+
+    }
+}
---
+++ Day1/dayOne.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace AdventOfCode.dayOne
+{
+    class dayOne
+    {
+        List<string> fuelInput = new List<string>();
+
+        public List<string> ImportInput()
+        {
+            fuelInput = File.ReadAllLines("input.txt").ToList();
+            return fuelInput;
+        }
+
+        public int calculateFuelPartTwo(List<String> FuelList)
+        {
+            var result = 0;
+            foreach (string moduel in FuelList)
+            {
+                var moduelNum = int.Parse(moduel);
+                while (true)
+                {
+                    var fuel = FuelAlgorithm(moduelNum);
+                    if (fuel <= 0)
+                    {
+                        break;
+                    }
+                    result += fuel;
+                    moduelNum = fuel;
+                }            
+            }
+            return result;
+        }
+
+        public int calculateFuelPartOne(List<String> FuelList)
+        {
+            var result = 0;
+            foreach (string moduel in FuelList){
+                result += FuelAlgorithm(int.Parse(moduel));
+            }
+            return result;
+        }
+
+        public int FuelAlgorithm(int mass)
+        {
+            return mass / 3 - 2; ;
+        }
+    }
+}
